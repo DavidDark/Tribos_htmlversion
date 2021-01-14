55848c13b42dd4fb96c3d4d229ac53d2b0db4cb0
DavidDark <link100david@hotmail.com>
2021-01-14 14:50:26 -0600

    Commit 14/01/21

---
+++ js/sliderb.js
@@ -0,0 +1,10 @@
+$('.hero-slider').owlCarousel({
+    animateOut: 'animate__fadeOutLeft',
+    animateIn: 'heartBeat',
+    loop:true,
+    nav:true,
+    margin:10,
+    items:1,
+    dots: true,
+    autoplay:500,
+})
